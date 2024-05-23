28f046db178aa67606058f6cfc93a61e4d6fc09b
AYUSH PATEL <138155010+shinnichi1312@users.noreply.github.com>
2024-05-23 16:18:05 +0530

    Update Appbar.tsx

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -63,7 +63,7 @@ function ProfileBox() {
   };
   return (
     <div className="relative cursor-pointer">
-      <Avatar name="Aadesh Kulkarni" onClick={() => setShow(!show)} />
+      <Avatar name="Ayush Patel" onClick={() => setShow(!show)} />
       {show && (
         <div className="absolute -bottom-24 -left-16 shadow-lg p-4 bg-gray-50 border border-gray-100 z-50 w-[160px]">
           <div className="flex flex-col gap-3">
